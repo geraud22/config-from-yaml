1b4e72141440db7cef2acece60e850d38ee3fbc0
Geraud WSL2 <geraud2201@gmail.com>
2025-04-16 11:16:42 +0200

    red

---
+++ config_test.go
@@ -0,0 +1,44 @@
+package cfy
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestLoadConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   []byte
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "expected behaviour",
+			input: []byte(`
+			app:
+				name: "Test"
+				version: "9.3"
+				mode: "dev"
+			`),
+			want: struct {
+				Name    string `mapstructure:"name"`
+				Version string `mapstructure:"version"`
+				Mode    string `mapstructure:"mode"`
+			}{},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := LoadConfig()
+			if tt.wantErr != (err != nil) {
+				t.Fatalf("%s failed. wantErr: %v, err: %v", tt.name, tt.wantErr, err)
+			}
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("%s mismatch. (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/geraud22/config-from-yaml
 
 go 1.22
 
-require github.com/spf13/viper v1.19.0
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/spf13/viper v1.19.0
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
